4f9283fc37e28d83f6da114064169b7a28d91464
Nejc Zupan <nejc.zupan@gmail.com>
2012-08-17 15:37:19 -0300

    skeleton for @@todo view

---
+++ src/collective/todoapp/browser/todo.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""A Folder view that lists Todo Items."""
+
+from five import grok
+from Products.ATContentTypes.interface import IATFolder
+
+# Search for templates in the current directory.
+# Hopefully line won't be needed in the future.
+grok.templatedir('.')
+
+
+class Todo(grok.View):
+    """A BrowserView to display the Todo listing on a Folder."""
+
+    grok.context(IATFolder)  # type of object on which this View is available
+    grok.require('zope2.View')  # what permission is needed for access
