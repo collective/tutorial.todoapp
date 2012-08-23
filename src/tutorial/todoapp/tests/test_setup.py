7b32b078b4030f85d5f19c142b4c6eac43755168
Nejc Zupan <nejc.zupan@gmail.com>
2012-08-23 16:33:53 -0300

    cleanup for chapter2

--- src/tutorial/todoapp/tests/test_setup.py
+++ src/tutorial/todoapp/tests/test_setup.py
@@ -36,53 +36,11 @@ class TestInstall(IntegrationTestCase):
         installer = api.portal.get_tool('portal_quickinstaller')
         self.assertTrue(installer.isProductInstalled('plone.app.dexterity'))
 
-    # types/Folder.xml
-    def test_folder_available_layouts(self):
-        """Test that our custom display layout (@@todo) is available on folder.
-
-        Also make sure that layouts that come with Plone out-of-the-box are
-        also still there.
-        """
-        layouts = self.portal.folder.getAvailableLayouts()
-        layout_ids = [id for id, title in layouts]
-
-        # out-of-the-box layouts are still there
-        self.assertIn('folder_listing', layout_ids)
-        self.assertIn('folder_summary_view', layout_ids)
-        self.assertIn('folder_tabular_view', layout_ids)
-        self.assertIn('atct_album_view', layout_ids)
-        self.assertIn('folder_full_view', layout_ids)
-
-        # our custom one
-        self.assertIn('todo', layout_ids)
-
-    # types.xml
+    # types/todo_item.xml
     def test_todo_item_installed(self):
         """Test that Todo Item content type is listed in portal_types."""
         types = api.portal.get_tool('portal_types')
         self.assertIn('todo_item', types.objectIds())
-
-    # workflows/todo_item_workflow/definition.xml
-    def test_todo_item_workflow_installed(self):
-        """"Test that todo_item_workflow is listed in portal_workflow."""
-        workflow = api.portal.get_tool('portal_workflow')
-        self.assertIn('todo_item_workflow', workflow.objectIds())
-
-    # workflows.xml
-    def test_todo_item_workflow(self):
-        """Test if todo_item workflow is to Todo Item content type."""
-        workflow = api.portal.get_tool('portal_workflow')
-        for portal_type, chain in workflow.listChainOverrides():
-            if portal_type in ('todo_item', ):
-                self.assertEquals(('todo_item_workflow',), chain)
-
-    # jsregistry.xml
-    def test_js_registered(self):
-        """Test that todoapp.js file is registered in portal_javascript."""
-        resources = self.portal.portal_javascripts.getResources()
-        ids = [r.getId() for r in resources]
-
-        self.assertIn('++resource++tutorial.todoapp/todoapp.js', ids)
 
 
 def test_suite():
