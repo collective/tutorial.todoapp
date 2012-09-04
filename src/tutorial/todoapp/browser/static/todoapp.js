7bde0e88bd72fd0056afcb641de05fd8e664d442
Nejc Zupan <nejc.zupan@gmail.com>
2012-09-04 11:19:45 +0200

    jslint

--- src/tutorial/todoapp/browser/static/todoapp.js
+++ src/tutorial/todoapp/browser/static/todoapp.js
@@ -1,47 +1,62 @@
-/*global $, document*/
+/*global $, document, alert*/
 (function () {
     "use strict";
 
-    function update_todo(todo_id, state, next_transition){
-        // TODO: use css instead of replacing images
+    function update_todo(todo_id, state, next_transition) {
+
+        var icon,
+            icon_url,
+            img_src,
+            link,
+            new_url,
+            url_parts;
 
         // update the state of the text
-        $('#'+todo_id+' td.todo-state').text(state);
+        $('#' + todo_id + ' td.todo-state').text(state);
         // change the next status url
-        var link = $('#'+todo_id+' td.todo-visual-status a');
-        var new_url = link.attr('href').split("=")[0] + "=" + next_transition;
-        link.attr('href', new_url); 
-        // update the image. 
-        var icon = $('#'+todo_id+' td.todo-visual-status img');
-        var icon_url = state + ".png";
-        var url_parts = icon.attr('src').split('/');
+        link = $('#' + todo_id + ' td.todo-visual-status a');
+        new_url = link.attr('href').split("=")[0] + "=" + next_transition;
+        link.attr('href', new_url);
+        // update the image.
+        icon = $('#' + todo_id + ' td.todo-visual-status img');
+        icon_url = state + ".png";
+        url_parts = icon.attr('src').split('/');
         // remove trailing image. nasty.
-        url_parts.splice(url_parts.length-1)
-        var img_src = url_parts.join("/") + "/" + icon_url; 
+        url_parts.splice(url_parts.length - 1);
+        img_src = url_parts.join("/") + "/" + icon_url;
         icon.attr('src', img_src);
     }
-    
-    $(document).ready(function(e){
-        $("table#todo-summary td.todo-visual-status a").click(function(event){
+
+    $(document).ready(function (e) {
+        $("table#todo-summary td.todo-visual-status a").click(function (event) {
+
+            var target,
+                url,
+                error_msg;
+
             // stop the browser from submitting after we know all is well
             event.preventDefault();
-            var target = $(this);
-            var url = target.attr('href');
-            var error_msg = "There was an error updating this item - please try again!"
+            target = $(this);
+            url = target.attr('href');
+            error_msg = "There was an error updating this item - please try again!";
             $.ajax({
                 url: url,
                 method: 'GET',
-                success: function(data){
-                    if (data.success){
-                        var state = data.results.state;
-                        var transition = data.results.transitions[0];
+                success: function (data) {
+
+                    var state,
+                        transition;
+
+                    if (data.success) {
+                        state = data.results.state;
+                        transition = data.results.transitions[0];
                         update_todo(target.closest('tr').attr('id'), state, transition);
                     } else {
-                        // This could be nicer in the future... 
+                        // This could be nicer in the future...
                         alert(error_msg + data.messages);
                     }
                 },
-                error: function(data){
+                error: function (data) {
                     alert(error_msg);
                 }
             });
